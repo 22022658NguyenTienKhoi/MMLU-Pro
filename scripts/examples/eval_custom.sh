4087a2fe3e6d443c1704d9d5a9f7bf8a2449b4b8
Nguyễn Tiến Khôi <127768809+22022658NguyenTienKhoi@users.noreply.github.com>
2025-07-30 09:00:04 +0700

    Update eval_custom.sh

--- scripts/examples/eval_custom.sh
+++ scripts/examples/eval_custom.sh
@@ -2,12 +2,12 @@
 
 save_dir="eval_results/"
 global_record_file="eval_results/eval_record_collection.csv"
-model="zerostratos/qwen-edu"
+model="qwen/qwen3-0.6B"
 selected_subjects="all"
-gpu_util=0.8
+gpu_util=0.9
 
 cd ../../
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1
 
 python evaluate_from_local.py \
                  --selected_subjects $selected_subjects \
